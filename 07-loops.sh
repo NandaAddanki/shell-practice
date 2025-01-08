509172df7b58a0aeac063d89d7dfdf50fa5320b0
Nanda <Nandu.addanki92@gmail.com>
2025-01-08 14:52:24 +0530

    loops

---
+++ 07-loops.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+for packege in $@
+do 
+dnf list  installed $package
+if [ $? -ne 0]
+ echo "install the $package"
+ else 
+echo "$package already installed"
+done
