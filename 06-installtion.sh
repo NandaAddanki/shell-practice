4299b841cc54467c518e658690c8e8a1d9a99fff
Nanda <Nandu.addanki92@gmail.com>
2025-01-08 13:07:13 +0530

    installation

--- 06-installtion.sh
+++ 06-installtion.sh
@@ -1,15 +1,15 @@
 
 #!/bin/bash
 
-userid=${id -u}
+userid=$(id -u)
 
-if [ ${userid} -ne 0]
+if [ ${userid} -ne 0 ]
 then
 echo "User should have root previliges to install"
 exit 1
 fi
 dnf list mysql
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then
 echo "mysql is not installed yet. installing"
 dnf install mysql -y
