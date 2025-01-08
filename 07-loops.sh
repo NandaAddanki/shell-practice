730bc9a2af238a5eff2aa4cb1ede3d86c6d91ff2
Nanda <Nandu.addanki92@gmail.com>
2025-01-08 16:32:15 +0530

    loops

--- 07-loops.sh
+++ 07-loops.sh
@@ -5,46 +5,46 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
-    then
-        echo "Please run this script with root priveleges"
-        exit 1
-    fi
-}
+# CHECK_ROOT(){
+#     if [ $USERID -ne 0 ]
+#     then
+#         echo "Please run this script with root priveleges"
+#         exit 1
+#     fi
+# }
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is...$R FAILED $N"
-        exit 1
-    else
-        echo -e "$2 is... $G SUCCESS $N"
-    fi
-}
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$2 is...$R FAILED $N"
+#         exit 1
+#     else
+#         echo -e "$2 is... $G SUCCESS $N"
+#     fi
+# }
 
-# for packege in $@
-# do 
-# dnf list  installed $package
-# if [ $? -ne 0 ]
-#  then
-#   echo "install the $package"
-#   dnf install $package -y
-#  else 
-#   echo "$package is already installed"
-# fi
-# done
-
-
-for package in $@ # $@ refers to all arguments passed to it
-do
-    dnf list installed $package
-    if [ $? -ne 0 ]
-    then
-        echo "$package is not installed, going to install it.."
-        dnf install $package -y
-        VALIDATE $? "Installing $package"
-    else
-        echo "$package is already installed..nothing to do"
-    fi
+for packege in $@
+do 
+dnf list  installed $package
+if [ $? -ne 0 ]
+ then
+  echo "install the $package"
+  dnf install $package -y
+ else 
+  echo "$package is already installed"
+fi
 done
+
+
+# for package in $@ # $@ refers to all arguments passed to it
+# do
+#     dnf list installed $package
+#     if [ $? -ne 0 ]
+#     then
+#         echo "$package is not installed, going to install it.."
+#         dnf install $package -y
+#         VALIDATE $? "Installing $package"
+#     else
+#         echo "$package is already installed..nothing to do"
+#     fi
+# done
