11fb382ec3ec4eedddc2d13b3899360feecc444e
Nanda <Nandu.addanki92@gmail.com>
2025-01-08 13:05:44 +0530

    installation

---
+++ 06-installtion.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+userid=${id -u}
+
+if [ ${userid} -ne 0]
+then
+echo "User should have root previliges to install"
+exit 1
+fi
+dnf list mysql
+if [ $? -ne 0]
+then
+echo "mysql is not installed yet. installing"
+dnf install mysql -y
+else
+echo "mysql already installed"
+fi
